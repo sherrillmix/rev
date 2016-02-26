da4256bfd8cda3dd954edd97054c14caddb1958a
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-02-26 12:16:36 -0500

    Flip query and subject

--- findIntronExons.R
+++ findIntronExons.R
@@ -9,8 +9,8 @@ onlyIntron<-setdiff(stack(intron),stack(exon),ignore.strand=TRUE)
 
 transcriptsByOverlapsByRegion<-function (x, ranges, maxgap = 0L, minoverlap = 1L, type = c("any", "start", "end"), ...){
 	trans<-transcripts(x, columns = c("tx_id", "tx_name"))
-	overlaps<-findOverlaps(trans, ranges, maxgap = maxgap, minoverlap = minoverlap, type = match.arg(type))
-	out<-tapply(subjectHits(overlaps)[1:1000],queryHits(overlaps)[1:1000],function(x)trans[x])
+	overlaps<-findOverlaps(ranges, trans, maxgap = maxgap, minoverlap = minoverlap, type = match.arg(type))
+	out<-tapply(subjectHits(overlaps),queryHits(overlaps),function(x)trans[x])
 	return(out)
 }
 
