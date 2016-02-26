b6b884b12adb3c17338479f255d0fa9aed3c06f5
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-02-26 12:02:25 -0500

    Faster overlap script

--- findIntronExons.R
+++ findIntronExons.R
@@ -1,25 +1,19 @@
+source("~/scripts/R/dna.R")
 
 library("TxDb.Hsapiens.UCSC.hg38.knownGene")
 library('GenomicRanges')
-library(parallel)
-
 exon<-exonsBy(TxDb.Hsapiens.UCSC.hg38.knownGene,use.names=TRUE)
 intron<-intronsByTranscript(TxDb.Hsapiens.UCSC.hg38.knownGene,use.names=TRUE)
 onlyExon<-setdiff(stack(exon),stack(intron),ignore.strand=TRUE)
 onlyIntron<-setdiff(stack(intron),stack(exon),ignore.strand=TRUE)
 
-transcriptByOverlapByRegion<-function (x, ranges, maxgap = 0L, minoverlap = 1L, type = c("any", "start", "end"), ...){
-	.local <- function (x, ranges, maxgap = 0L, minoverlap = 1L, type = c("any", "start", "end"), columns = c("tx_id", "tx_name")){ 
-		findOverlaps(transcripts(x, columns = columns), ranges, maxgap = maxgap, minoverlap = minoverlap, type = match.arg(type))
-		browser()
-	}
-	.local(x, ranges, maxgap, minoverlap, type, ...)
+transcriptsByOverlapsByRegion<-function (x, ranges, maxgap = 0L, minoverlap = 1L, type = c("any", "start", "end"), ...){
+	trans<-transcripts(x, columns = c("tx_id", "tx_name"))
+	overlaps<-findOverlaps(trans, ranges, maxgap = maxgap, minoverlap = minoverlap, type = match.arg(type))
+	out<-tapply(subjectHits(overlaps)[1:1000],queryHits(overlaps)[1:1000],function(x)trans[x])
+	return(out)
 }
 
-overlappingIntronGenes<-lapply(split(onlyIntron,1:length(onlyIntron))[1:1000],function(intron){if(runif(1)<.001)cat('.');transcriptsByOverlaps(TxDb.Hsapiens.UCSC.hg38.knownGene,intron)},mc.cores=12)
-overlappingExonGenes<-mclapply(split(onlyExon,1:length(onlyExon)),function(exon){if(runif(1)<.001)cat('.');transcriptsByOverlaps(TxDb.Hsapiens.UCSC.hg38.knownGene,exon)},mc.cores=12)
+overlappingIntronGenes<-cacheOperation('work/overlappingIntronGenes.Rdat',transcriptsByOverlapsByRegion,TxDb.Hsapiens.UCSC.hg38.knownGene,onlyIntron,EXCLUDE='TxDb.Hsapiens.UCSC.hg38.knownGene')
+overlappingExonGenes<-cacheOperation('work/overlappingExonGenes.Rdat',transcriptsByOverlapsByRegion,TxDb.Hsapiens.UCSC.hg38.knownGene,onlyExon,EXCLUDE='TxDb.Hsapiens.UCSC.hg38.knownGene')
 
-
-	overlappingGenes<-findOverlaps(onlyIntron,geneRanges)
-	overlappingExonGenes<-findOverlaps(onlyExon,geneRanges)
-#library("Homo.sapiens")
