55fde7145a77956e22d051454ef2ad5207e03237
sherrillmix <sherrillmix@users.noreply.github.com>
2016-08-30 16:11:07 -0400

    Specify strand specific

--- runCufflinks.bash
+++ runCufflinks.bash
@@ -4,5 +4,5 @@ baseDir=work/align
 tmpDir=/tmp
 echo "Running cuffdiff"
 diffDir=work/diff
-~/installs/cufflinks/cuffdiff --max-bundle-frags 100000000 -o $diffDir -p 16 --min-reps-for-js-test 1 -v ~/installs/star/index/knownGene.gtf work/align/control_1.bam,work/align/control_1.bam work/align/rev_1.bam,work/align/rev_2.bam
+~/installs/cufflinks/cuffdiff --max-bundle-frags 100000000 -o $diffDir -p 16 --min-reps-for-js-test 1 -v --library-type fr-firststrand ~/installs/star/index/knownGene.gtf work/align/control_1.bam,work/align/control_1.bam work/align/rev_1.bam,work/align/rev_2.bam
 
