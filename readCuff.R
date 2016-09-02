765ce5198f60fe0e8049aa40632e5d2c6dcc9322
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-02 07:28:18 -0400

    Add ensembl and ncbi ids

--- readCuff.R
+++ readCuff.R
@@ -3,12 +3,21 @@ dataType<-'gene'
 geneDiffFile<-sprintf('%s/%s_exp.diff',dataDir,dataType)
 outFile<-sprintf('out/%s_exp.diff.gz',dataType)
 geneDiff<-read.table(geneDiffFile,header=TRUE,stringsAsFactors=FALSE)
-kgXref<-read.table('~/installs/star/kgXRef.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='')
-rownames(kgXref)<-kgXref$V1
-geneDiff$name<-kgXref[geneDiff$gene_id,'V5']
-kgAlias<-read.table('~/installs/star/kgAlias.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='')
-kgAlias<-kgAlias[kgAlias$V1!=kgAlias$V2,]
-aliases<-tapply(kgAlias$V2,kgAlias$V1,paste,collapse='|')
+kgXref<-read.table('~/installs/star/kgXRef.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='',header=TRUE)
+rownames(kgXref)<-kgXref$X.kgID
+geneDiff$name<-kgXref[geneDiff$gene_id,'geneSymbol']
+geneDiff$ncbi<-kgXref[geneDiff$gene_id,'mRNA']
+kgAlias<-read.table('~/installs/star/kgAlias.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='',header=TRUE)
+kgAlias<-kgAlias[kgAlias$X.kgID!=kgAlias$alias,]
+aliases<-tapply(kgAlias$alias,kgAlias$X.kgID,paste,collapse='|')
 geneDiff$name[is.na(geneDiff$name)]<-aliases[geneDiff$gene_id[is.na(geneDiff$name)]]
+knownToEnsembl<-read.table('~/installs/star/knownToEnsembl.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='',header=TRUE)
+rownames(knownToEnsembl)<-knownToEnsembl$X.name
+geneDiff$ensembl<-knownToEnsembl[geneDiff$gene_id,'value']
+knownToKeggEntrez<-read.table('~/installs/star/knownToKeggEntrez.tsv.gz',sep='\t',stringsAsFactors=FALSE,quote='',comment='',header=TRUE)
+knownToKeggEntrez$entrez<-sub('^.*\\+','',knownToKeggEntrez$keggEntrez)
+knownToKeggEntrez<-unique(knownToKeggEntrez[,c('X.name','entrez')])
+rownames(knownToKeggEntrez)<-knownToKeggEntrez$X.name
+#geneDiff$entrez<-knownToKeggEntrez[geneDiff$gene_id,'entrez']
 write.csv(geneDiff,gzfile(outFile))
 
